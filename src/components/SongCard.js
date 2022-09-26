638306cd107ba09d205a377e21b7c1f560545b42
Namita Mathew <namitannie@gmail.com>
2022-09-25 23:58:01 -0400

    fix ui

--- src/components/SongCard.js
+++ src/components/SongCard.js
@@ -6,7 +6,8 @@ export default class SongCard extends React.Component {
 
         this.state = {
             isDragging: false,
-            draggedTo: false
+            draggedTo: false,
+            hover: false
         }
     }
     handleDragStart = (event) => {
@@ -47,12 +48,36 @@ export default class SongCard extends React.Component {
         
         this.setState(prevState => ({
             isDragging: false,
-            draggedTo: false
+            draggedTo: false,
+            hover: true
         }));
 
         // ASK THE MODEL TO MOVE THE DATA
         this.props.moveCallback(sourceId, targetId);
     }
+
+    handleHoverStart = (event) => {
+        event.preventDefault();
+        this.setState(prevState => ({
+            isDragging: prevState.isDragging,
+            draggedTo: prevState.draggedTo,
+            hover: true
+        }))
+        console.log("hover")
+
+    }
+
+    handleHoverEnd = (event) => {
+        event.preventDefault();
+        this.setState(prevState => ({
+            isDragging: prevState.isDragging,
+            draggedTo: prevState.draggedTo,
+            hover: false
+        }))
+        console.log("hoverStop")
+
+    }
+
 
     getItemNum = () => {
         return this.props.id.substring("playlist-song-".length);
@@ -60,11 +85,36 @@ export default class SongCard extends React.Component {
 
     render() {
         const { song } = this.props;
+
         let num = this.getItemNum();
         console.log("num: " + num);
         let itemClass = "playlister-song";
-        if (this.state.draggedTo) {
-            itemClass = "playlister-song-dragged-to";
+        if (this.state.hover) {
+            itemClass = "playlister-song-hover";
+            if (this.state.draggedTo) {
+                itemClass = "playlister-song-dragged-to";
+            }
+            return (
+                <div
+                id={'song-' + num}
+                className={itemClass}
+                onDragStart={this.handleDragStart}
+                onDragOver={this.handleDragOver}
+                onDragEnter={this.handleDragEnter}
+                onDragLeave={this.handleDragLeave}
+                onDrop={this.handleDrop}
+                onMouseEnter={this.handleHoverStart}
+                onMouseLeave={this.handleHoverEnd}
+                draggable="true"
+            >
+                {num}.&nbsp;<a href={"https://youtu.be/"+ song.youTubeId}> {song.title} by {song.artist}</a>
+                <input
+                    type="button"
+                    id={"delete-list-" + num}
+                    className="song-card-button"
+                    value={"\u2715"} />
+            </div>
+            )
         }
         return (
             <div
@@ -75,9 +125,17 @@ export default class SongCard extends React.Component {
                 onDragEnter={this.handleDragEnter}
                 onDragLeave={this.handleDragLeave}
                 onDrop={this.handleDrop}
+                onMouseEnter={this.handleHoverStart}
+                onMouseLeave={this.handleHoverEnd}
                 draggable="true"
             >
-                {song.title} by {song.artist}
+                {num}.&nbsp;<a href={"https://youtu.be/" + song.youTubeId}> {song.title} by {song.artist}</a>
+                
+                <input
+                    type="button"
+                    id={"delete-list-" + num}
+                    className="song-card-button"
+                    value={"\u2715"} />
             </div>
         )
     }
